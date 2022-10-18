d1ac68a629b293d45b7df5ca95c0e83a39be47b1
Stanley <stanleyushi@gmail.com>
2022-10-18 11:30:33 -0700

    Adding numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Printing natural numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int i, sum = 0;
+
+for (i = 0; i < 1024; i++)
+{
+if ((i % 5) == 0 || (i % 3) == 0)
+sum += 1;
+}
+printf("%d\n", sum);
+
+return (0);
+}
