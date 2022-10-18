d9e5f2a609ed27d7e2d14997b5d26371509f437b
Stanley <stanleyushi@gmail.com>
2022-10-18 14:13:01 -0700

    Printing n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,8 @@ if (n >= 0 && n <= 15)
 for (num = 0; num <= n; num++)
 {
 _putchar('0');
-for (mult = 1; mult <= n; mult++)
+
+for (multi = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
@@ -29,7 +30,7 @@ _putchar(' ');
 if (prod >= 100)
 {
 _putchar((prod / 100) + '0');
-_putchar(((prod / 100)) % 10 + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
