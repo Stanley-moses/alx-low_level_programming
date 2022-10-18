49315776ec27579eba79eb161a33ba815ebdb8a2
Stanley <stanleyushi@gmail.com>
2022-10-18 02:15:04 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
