625ede0f254d1abf4826f466c3726ce2df80d45f
Stanley <stanleyushi@gmail.com>
2022-10-18 04:40:51 -0700

    Using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
-* main - Check description
+* main - Prints _putchar
 * Description: It prints the word _putchar, followed by a new line
 * Return: 0
 */
+
 int main(void)
 {
-char word[8] = "_putchar";
-int i;
-
-for (i = 0; i < 8; i++)
-putchar(word[i]);
-putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
