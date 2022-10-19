c0a6733ae8003f2bceb8aed35f8f7d4ef815627b
Stanley <stanleyushi@gmail.com>
2022-10-19 14:45:10 -0700

    Printing remaining day

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,31 +1,19 @@
 #include "main.h"
 
-
-
 /**
+* positive_or_negative - Checks if integer is positive or negative
+* Return: 0
+*/
 
- * positive_or_negative - tests function that prints if integer is positive or negative
-
- * Return: 0
-
- */
-
-
-void positive_or_negative(void)
-
-  
+void positive_or_negative(int i)
 {
-  
-  int i;
-  
 
-  
-  i = 0;
-  
-  positive_or_negative(i);
-  
+int i;
 
-  
-  return (0);
-  
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
 }
