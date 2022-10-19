c353189a8af039a82c13f8a9a0db153234977ae2
Stanley <stanleyushi@gmail.com>
2022-10-19 07:50:02 -0700

    Commenting

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+
+ * positive_or_negative - tests function that prints if integer is positive or negative
+
+ * Return: 0
+
+ */
+
+
+void positive_or_negative(void)
+
+  
+{
+  
+  int i;
+  
+
+  
+  i = 0;
+  
+  positive_or_negative(i);
+  
+
+  
+  return (0);
+  
+}
