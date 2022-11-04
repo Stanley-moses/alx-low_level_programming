d35a456e38db7922936b46d7c79686b0c49ca1cf
Stanley <stanleyushi@gmail.com>
2022-11-04 02:41:07 -0700

    Adding numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - a function that adds numbers
+* @argc: argument count
+* @argv: argument
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+int a, b, sum = 0;
+char *flag;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (a = 1; argv[a]; a++)
+{
+b = strtol(argv[a], &flag, 10);
+if (*flag)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+sum += b;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
