9cd6f9f730384a623a9b5ed5517645af20af4314
Stanley <stanleyushi@gmail.com>
2022-11-04 02:57:49 -0700

    Hard things are hard

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - a function that prints coins to make change
+* @argc: argument count
+* @argv: argument
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+int total, count;
+unsigned int a;
+char *p;
+int cents[] = {25, 10, 5, 2};
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+total = strtol(argv[1], &p, 10);
+count = 0;
+if (!*p)
+{
+while (total > 1)
+{
+for (a = 0; a < sizeof(cents[a]); a++)
+{
+if (total >= cents[a])
+{
+count += total / cents[a];
+total = total % cents[a];
+}
+}
+}
+if (total == 1)
+count++;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", count);
+return (0);
+}
