89ae62032209dcd658d2b9229577637c3f9018d3
Stanley <stanleyushi@gmail.com>
2022-11-10 06:10:16 -0800

    The beginning of hard things

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 #include <stdio.h>
