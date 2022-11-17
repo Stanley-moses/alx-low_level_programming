8ecb04da710d10dfcbccd6661f55bc4115298230
Stanley <stanleyushi@gmail.com>
2022-11-17 04:16:52 -0800

    On hard things

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -4,26 +4,28 @@
 * print_strings - a function that prints strings
 * @separator: a string that separates strings
 * @n: number of parameters
+* @...: other parameters
 * Return: 0
 */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 char *str;
-unsigned int a;
+unsigned int a = 0;
 va_list ap;
 va_start(ap, n);
-if (separator == NULL)
-separator = "";
-for (a = 0; a < n; a++)
+for (; a < n; a++)
 {
 str = va_arg(ap, char*);
 if (str == NULL)
-str = "(nil)";
+printf("(nil)");
+else
 printf("%s", str);
+if (separator == NULL)
+continue;
 if (a < n - 1)
 printf("%s", separator);
 }
-printf("\n");
+printf('\n');
 va_end(ap);
 }
