0b5f6a748df9b2029410435310d02cff86ab207f
Stanley <stanleyushi@gmail.com>
2022-11-24 05:10:10 -0800

    Constructing attribute

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
+/* Using constructors in C */
+
+void main_constructor(void) __attribute__((constructor));
 /**
-* myStartupFun - a function that applies the constructor
+* main_constructor - a function that prints a message
+* Return: Nothing
 */
 
-void myStartupFun(void) __attribute__ ((constructor))
-
-/**
-* myStartupFun - an implementation of function
-*/
-
-void myStartupFun(void)
+void main_constructor(void)
 {
 printf("You're beat! and yet, you must allow, \n");
 printf("I bore my house upon my back!\n");
