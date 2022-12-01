384bd32b591c33b1e4e9a8a6f04d30adccb52d65
Stanley-moses <stanleyushi@gmail.com>
2022-12-01 05:33:50 -0800

    Adding bits at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 unsigned long int a;
 if (index > (sizeof(unsigned long int) * 8 - 1))
 return (-1);
-a = -(1 << index);
+a = ~(1 << index);
 *n = *n & a;
 return (1);
 }
