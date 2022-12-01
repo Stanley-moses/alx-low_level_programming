ef21d59fac1750f992bf44943b314d18840e82b4
Stanley-moses <stanleyushi@gmail.com>
2022-12-01 05:29:28 -0800

    Adding bits at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* clear_bit - a function that sets value of bit at given index
+* @n: number
+* @index: index to set bits
+* Return: 1 on success, otherwise -1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int a;
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+a = -(1 << index);
+*n = *n & a;
+return (1);
+}
